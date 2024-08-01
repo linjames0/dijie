2e6071594e9ca7c9d34c37417ae915444fbca9c8
James <james218.lin@gmail.com>
2024-08-01 10:39:14 -0700

    added elements

--- src/app/page.js
+++ src/app/page.js
@@ -4,26 +4,26 @@ import ProductInfo from '../components/productInfo';
 export default function Home() {
     return (
         <div>
-            <div>
-                <img class="relative" src="/images/landing2.jpg" alt="landing1" />
-                <p class="text-blue-300 text-center mt-10 mb-5">NEW</p>
-                <h1 class="text-5xl text-center">dijie shooter</h1>
-                
-                <div class="h-10"></div>
-                <hr class="border-gray-600"/>
+            <img class="relative" src="/images/landing2.jpg" alt="landing1" />
+            <p class="text-blue-300 text-center mt-10 mb-5">NEW</p>
+            <h1 class="text-5xl text-center">dijie shooter</h1>
+            
+            <div class="h-10"></div>
+            <hr class="border-gray-600"/>
 
-                <p class="text-sm text-gray-400 p-3">START TAKING DIJIES</p>
+            <p class="text-sm text-gray-400 p-3">START TAKING DIJIES</p>
 
-                {/* <hr /> */}
-                <div class="flex items-center justify-between p-3 pt-0">
-                    <p>SELECT COLOR</p>
-                    <div class="flex items-center">
-                        <ColorButton />
-                    </div>
+            {/* <hr /> */}
+            <div class="flex items-center justify-between p-3 pt-0">
+                <p>SELECT COLOR</p>
+                <div class="flex items-center">
+                    <ColorButton />
                 </div>
-
-                <ProductInfo />
             </div>
+            <button className="w-full mb-5 text-black bg-blue-300">BUY NOW</button>
+
+            <ProductInfo />
+            <div className="h-16"/>
         </div>
     );
 }
--- src/components/productInfo.js
+++ src/components/productInfo.js
@@ -7,9 +7,9 @@ export default function ProductInfo() {
     const [openIndex, setOpenIndex] = useState(null);
 
     const faqs = [
-        { question: "What is Next.js?", answer: "Next.js is a React framework for production." },
-        { question: "What is Tailwind CSS?", answer: "Tailwind CSS is a utility-first CSS framework for rapid UI development." },
-        { question: "How to use Tailwind with Next.js?", answer: "You can install Tailwind CSS in a Next.js project and configure it to work with your components." },
+        { question: "FREE SHIPPING & RETURNS", answer: "Next.js is a React framework for production." },
+        { question: "90 DAY WARRANTY", answer: "Tailwind CSS is a utility-first CSS framework for rapid UI development." },
+        { question: "TECH SPECS", answer: "You can install Tailwind CSS in a Next.js project and configure it to work with your components." },
     ];
 
     const toggleFaq = (index) => {
@@ -18,23 +18,26 @@ export default function ProductInfo() {
 
     return (
         <div>
-            <div className={`"max-w-2xl m-2 p-4" ${styles.accordion}`}>
-            <hr/>
-            {faqs.map((faq, index) => (
-                <div key={index} className="mb-4">
-                <button
-                    onClick={() => toggleFaq(index)}
-                    className={styles.question}
-                >
-                    {faq.question}
-                </button>
-                <div className={`${styles.answer} ${openIndex === index ? 'block' : 'hidden'}`}>
-                    {faq.answer}
-                </div>
-                <hr/>
-                </div>
-            ))}
+            <div className={`"max-w-2xl p-4" ${styles.accordion}`}>
+                <hr className="border-gray-600"/>
+                {faqs.map((faq, index) => (
+                    <div key={index}>
+                        <button
+                            onClick={() => toggleFaq(index)}
+                            className={styles.question}
+                        >
+                            {faq.question}
+                        </button>
+                        <div className={`${styles.answer} ${openIndex === index ? 'block' : 'hidden'}`}>
+                            {faq.answer}
+                        </div>
+                        <hr className="border-gray-600"/>
+                    </div>
+                ))}
             </div>
+            <button className="my-1 mx-3">SHARE PRODUCT PAGE</button>
+            <hr className="border-gray-600"/>
+
         </div>
     );
 }
